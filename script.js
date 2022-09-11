29d28bd1cace6ca186774e9dcca2cf4f5b693c5b
Akshay Raichur <lordakshay99@gmail.com>
2022-09-11 20:15:36 +0530

    feat: app completed

---
+++ script.js
@@ -0,0 +1,46 @@
+const checkAnswer = document.querySelector('.answer-btn');
+const luckyNumber = document.querySelector('.lucky-number');
+const birthDate = document.querySelector('.date-picker');
+const result = document.querySelector('.result');
+
+const calculateSum = (val) => {
+  let sum = 0;
+
+  while (val !== 0) {
+    let rem = val % 10;
+    sum += rem;
+    val = parseInt(val / 10);
+  }
+
+  return sum;
+};
+
+const calculateResult = () => {
+  let luckyNum = parseInt(luckyNumber.value);
+  let bDate = birthDate.value;
+
+  let message = '';
+
+  if (luckyNum === 0 || isNaN(luckyNum) || bDate === '') {
+    message = 'Ahh, give a proper response dude 👿';
+    result.textContent = message;
+    return;
+  }
+
+  bDate = parseInt(bDate.replaceAll('-', ''));
+  let sumOfBirthDate = calculateSum(bDate);
+
+  if (sumOfBirthDate % luckyNum === 0) {
+    message = `Yay!! You are lucky. Congratulations, ${luckyNum} is your lucky number 🔥🎉`;
+  } else {
+    message = `Ahh!! ${luckyNum} is not that lucky for you. 😔`;
+  }
+
+  result.textContent = message;
+};
+
+function App() {
+  checkAnswer.addEventListener('click', calculateResult);
+}
+
+App();
